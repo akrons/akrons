ca9b5372141518b3bae016ecf47f1d9b1e020f14
rudxde <kj4derechte@gmail.com>
2020-12-15 14:30:26 +0100

    feat(cms): subscribe to router updates and load static routes

--- client/cms/projects/cms/src/lib/components/page/page.component.ts
+++ client/cms/projects/cms/src/lib/components/page/page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ContentService } from '../../services/content.service';
-import { Observable, of } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { Observable, of, Subject, combineLatest } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, share, take, takeUntil } from 'rxjs/operators';
 import { TitleService } from '@akrons/core';
 import { cms } from '@akrons/types';
 
@@ -11,30 +11,69 @@ import { cms } from '@akrons/types';
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.css']
 })
-export class PageComponent implements OnInit {
+export class PageComponent implements OnInit, OnDestroy {
 
+  @Input()
+  changeTitle?: boolean;
+
+  @Input()
+  loadRoute?: string;
+
+  destroy$: Subject<void> = new Subject();
   elements: Observable<cms.IPayload[]>;
 
   constructor(
     private contentService: ContentService,
     private route: ActivatedRoute,
-    private titleService: TitleService
+    private titleService: TitleService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
-    const activatedRouteSnapshot = this.route.snapshot;
-    if (activatedRouteSnapshot.data['template']) {
-      const templatePage: cms.IPage = activatedRouteSnapshot.data['template'];
-      this.elements = of(templatePage.elements);
-      this.titleService.setTitle(templatePage.title);
+    if (this.loadRoute) {
+      this.loadPageFromRoute(this.loadRoute);
       return;
     }
-    const route = activatedRouteSnapshot.url.map(x => x.toString()).join('/');
-    const page = this.contentService.loadPage(route);
+    combineLatest([
+      this.route.url,
+      this.route.data,
+    ]).pipe(
+      debounceTime(10),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    ).subscribe({
+      next: ([url, data]) => {
+        if (data['template']) {
+          const templatePage: cms.IPage = data['template'];
+          this.elements = of(templatePage.elements);
+          this.setTitle(templatePage.title);
+          return;
+        }
+        const route = url.map(x => x.toString()).join('/');
+        this.loadPageFromRoute(route);
+      }
+    });
+  }
+
+  private loadPageFromRoute(route: string) {
+    const page = this.contentService.loadPage(route).pipe(share(), takeUntil(this.destroy$));
     this.elements = page.pipe(map(x => x.elements));
-    page.pipe(take(1)).subscribe({next: x => {
-      this.titleService.setTitle(x.title);
-    }});
+    page.pipe(take(1)).subscribe({
+      next: x => {
+        this.setTitle(x.title);
+      }
+    });
+  }
+
+  setTitle(title: string): void {
+    if (this.changeTitle) {
+      this.titleService.setTitle(title);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
